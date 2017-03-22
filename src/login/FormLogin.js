f5be656a05eb1954790a1eb66498fe13ef353cc8
autsakorn <autsakorn@Autsakorns-MacBook-Pro.local>
2017-03-22 17:12:43 +0700

    edit formlogin

--- src/login/FormLogin.js
+++ src/login/FormLogin.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
  class FormLogin extends Component{
    constructor(props){
      super(props)
+     this.state = {...props};
    }
    render(){
      return(
