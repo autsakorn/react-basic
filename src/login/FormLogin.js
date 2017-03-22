fedb5cef9388ee16789c62b23a685c56e8e5fa3c
Sarinaphat <kaew.knn@gmail.com>
2017-03-22 17:26:16 +0700

    ssss

--- src/login/FormLogin.js
+++ src/login/FormLogin.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
  class FormLogin extends Component{
    constructor(props){
      super(props)
+     this.state = {...props};
    }
    render(){
      return(
        <div className="login">
          <div className="login-screen">
             <div className="app-title">
-              <h3>vSpace</h3>
+              <h1>vSpace</h1>
               <div className="control-group">
               <input type="text" className="Username" placeholder="Username"/>
               </div>
