aaa08b307544b52d36e36ad88b273743f4a129a5
autsakorn <autsakorn@Autsakorns-MacBook-Pro.local>
2017-03-22 14:39:24 +0700

    Add Welcome

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './projectplan/App';
+import Url from './config/url';
+import InfoGen from './config/InfoGen';
 import './index.css';
-
+import Welcome from './login/Welcome';
 
 var formData = new FormData();
-formData.append('token','e50b57a63b0527a3dac3015dd3b29c4e');
-formData.append('email','autsakorn.t@firstlogic.co.th');
-var urlInfo = "http://vspace.in.th/apis/v1/user/header";
-var urlProjectDetail = "http://vspace.in.th/apis/v1/projectplan/projectdetail";
-var urlListCaseAll = "http://vspace.in.th/apis/v1/casetype/listCaseAll";
 
-get(urlInfo, formData).then(function(resInfo){
-  formData.append('project_sid',250);
-  get(urlProjectDetail, formData).then(function(resPd){
-    get(urlListCaseAll, formData).then(function(resLCA){
-      ReactDOM.render(
-          <App info={resInfo.data} projectInfo={resPd.project_detail.project_detail} casetype={resPd.data} listType={resLCA.data}/>, document.getElementById('root')
-      );
+if(InfoGen.token){
+  formData.append('token',InfoGen.token);
+  formData.append('email',InfoGen.email);
+  get(Url.urlInfo, formData).then(function(resInfo){
+    formData.append('project_sid',250);
+    get(Url.urlProjectDetail, formData).then(function(resPd){
+      get(Url.urlListCaseAll, formData).then(function(resLCA){
+        ReactDOM.render(
+            <App info={resInfo.data} projectInfo={resPd.project_detail.project_detail} casetype={resPd.data} listType={resLCA.data}/>, document.getElementById('root')
+        );
+      });
     });
+  },function(error){
+    console.log(error);
   });
-},function(error){
-  console.log(error);
-});
+}else{
+  ReactDOM.render(
+    <Welcome />, document.getElementById('root')
+  );
+}
 
 function get(url, formData){
   return new Promise(function(resolve, reject){
